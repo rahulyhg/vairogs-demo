515468d89f9918e2eda57a07303d3c6759da7412
Dāvis Zālītis <davis.zalitis@lattelecom.lv>
2018-11-07 18:32:29 +0200

    added cache

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\CacheKernel;
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
@@ -26,6 +27,12 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 }
 
 $kernel = new Kernel($env, $debug);
+
+if ('prod' === $env) {
+    $kernel = new CacheKernel($kernel);
+}
+
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
