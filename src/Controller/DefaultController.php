d7608b2f6b2699a56e29b6b9066bf02f7a030b7b
Unknown <k0d3r1s@gmail.com>
2018-11-21 23:07:16 +0200

    configuration update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,21 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Phpsed\Cache\Annotation\Cache;
 use Vairogs\Utils\Core\Util\StreamResponse;
-use Vairogs\Utils\Guzzle\GuzzleHttp\Client as Guzzle;
+use App\Service\Guzzle;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
-     * @Cache()
      */
     public function index(Guzzle $guzzle)
     {
+        $guzzle->get('http://worldclockapi.com/api/json/est/now');
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController'
+            'controller_name' => 'DefaultController',
+            'rand' => \random_int(1, 100)
         ]);
     }
 }
---
+++ src/Service/Guzzle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service;
+
+use GuzzleHttp\Client;
+
+class Guzzle extends Client
+{
+
+}
